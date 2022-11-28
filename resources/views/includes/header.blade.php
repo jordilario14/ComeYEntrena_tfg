69da86a705f8c478e6f16baeda0050f6e5d830e3
Jordi Segura Lario <jordilario@uoc.edu>
2022-11-28 21:49:05 +0100

    Login diseñado

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,24 +1,113 @@
-<nav class="navbar navbar-expand-lg navbar-light golmar-header">
-    <div class="container-fluid margin-left-5 margin-right-5">
-        <div>
-            <span href="{{ route('index') }}" class="title-main">
-                Come y Entrena
-            </span>
-        </div>
-        <div>
-            @guest
-                <span class="logIn" id="login-link">
-                    Iniciar sesión
-                </span>
-            @endguest
-
-            @auth
-                DD   
-            @endauth
-        </div>
+{{-- <nav class="header-cye">
+  <div class="container-fluid margin-left-5 margin-right-5">
+    <div>
+      <span href="{{ route('index') }}" class="title-main">
+          Come y Entrena
+      </span>
+   
     </div>
+  </div>
+
+
+  <div class="separated-bt">
+    <div>
+      <span class="logIn pr2">
+        Inicio
+      </span>
+
+      <span class="logIn plr2">
+        Sobre nosotros
+      </span>
+
+      <span class="logIn pl2">
+        Dónde estamos
+      </span>
+    </div>
+    <div>
+      @guest
+        <span class="logIn" id="login-link">
+          Iniciar sesión
+        </span>
+      @endguest
+    </div>
+  </div>
+  <hr class="hr-separador">
+
 </nav>
 
+ --}}
+
+ <nav class="navbar-expand-lg navbar-light header-cye">
+  <div class="container-fluid margin-left-5 margin-right-5 separated-flex">
+
+    <div>
+      <span href="{{ route('index') }}" class="title-main">
+        Come y Entrena
+      </span>
+    </div>
+
+    <div>
+      <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+
+ </div>
+ <div class="separated-bt collapse navbar-collapse " id="navbarNavDropdown">
+  <ul class="navbar-nav me-auto mb-2 mb-lg-0 login-ul">
+    <li class="nav-item pr2">
+      <span class="logIn">
+        Inicio
+      </span>
+    </li>
+    <li class="nav-item plr2">
+      <span class="logIn">
+        Sobre nosotros
+      </span>
+    </li>
+    <li class="nav-item pl2">
+      <span class="logIn ">
+        Dónde estamos
+      </span>
+    </li>
+  </ul>
+  @guest
+  <span class="logIn" id="login-link">
+    Iniciar sesión
+  </span>
+@endguest
+</div>
+{{--  <div class="separated-bt collapse navbar-collapse" id="navbarNavDropdown">
+  <div>
+    <ul class="navbar-nav login-ul">
+      <li class="nav-item pr2">
+        <span class="logIn">
+          Inicio
+        </span>
+      </li>
+      <li class="nav-item plr2">
+        <span class="logIn">
+          Sobre nosotros
+        </span>
+      </li>
+      <li class="nav-item pl2">
+        <span class="logIn ">
+          Dónde estamos
+        </span>
+      </li>
+
+    </ul>
+
+    @guest
+      <span class="logIn navbar-text" id="login-link">
+        Iniciar sesión
+      </span>
+    @endguest
+  </div>
+</div> --}}
+<hr class="hr-separador">
+
+</nav>
 
 
 <div class="modal" tabindex="-1" id="login-modal">
@@ -53,5 +142,3 @@
     </div>
   </div>
 </div>
-
-<hr class="hr-separador">
